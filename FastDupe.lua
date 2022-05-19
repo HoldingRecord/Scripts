635120d280557ecfc2634cb3ef382da1054e8077
HoldingRecord <91856885+HoldingRecord@users.noreply.github.com>
2022-05-19 00:38:31 -0400

    Update FastDupe.lua

--- FastDupe.lua
+++ FastDupe.lua
@@ -1,25 +1,23 @@
 for i = 1, tonumber(1) or 1 do
-for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-      if v.ClassName == "Tool" then
-      v.Parent = game.Players.LocalPlayer.Character
-      v.Parent = workspace
-      amongus = game:GetService('Players').LocalPlayer
-      prev = amongus.Character:WaitForChild('HumanoidRootPart').CFrame
-      amongus.Character.Parent = workspace.Terrain
-      amongus.Character:Destroy()
-        game.Players.LocalPlayer.Character = nil
-        game.Players.LocalPlayer.CharacterAdded:Wait()
-        amongus = game.Players.LocalPlayer.Character
-        amongus:WaitForChild("Humanoid")
-        amongus:WaitForChild('HumanoidRootPart').CFrame = prev
-        game.Players.LocalPlayer.CharacterAdded:Wait()
-        amongus = game.Players.LocalPlayer.Character
-        amongus:WaitForChild("Humanoid")
-        for i,v in pairs(workspace:GetChildren()) do
-            if v:IsA("Tool") then
-                v.Parent = game.Players.LocalPlayer.Backpack
+    for i, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+        if v.ClassName == "Tool" then
+            v.Parent = game.Players.LocalPlayer.Character
+            v.Parent = workspace
+            prev = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame
+            game.Players.LocalPlayer.Character.Parent = workspace.Terrain
+            game.Players.LocalPlayer.Character:Destroy()
+            game.Players.LocalPlayer.Character = nil
+            game.Players.LocalPlayer.CharacterAdded:Wait()
+            game.Players.LocalPlayer.Character:WaitForChild("Humanoid")
+            game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = prev
+            game.Players.LocalPlayer.CharacterAdded:Wait()
+            amongus = game.Players.LocalPlayer.Character
+            game.Players.LocalPlayer.Character:WaitForChild("Humanoid")
+            for i, v in pairs(workspace:GetChildren()) do
+                if v:IsA("Tool") then
+                    v.Parent = game.Players.LocalPlayer.Backpack
+                end
             end
         end
-end
-end
+    end
 end
